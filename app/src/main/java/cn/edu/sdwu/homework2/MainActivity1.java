fadec70bb895e31029a8f5b64115ef69fdbbe1c1
170507180225 <894993977@qq.com>
2020-05-07 17:30:56 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/homework2/MainActivity1.java
@@ -0,0 +1,20 @@
+package cn.edu.sdwu.homework2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+    public void homeworkOne(View view){
+        //界面跳转
+        Intent intent=new Intent(this,MusicActivity.class);
+        startActivity(intent);
+    }
+}
